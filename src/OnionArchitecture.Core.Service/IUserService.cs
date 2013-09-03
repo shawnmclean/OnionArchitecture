d190c614d054537d6ce2feb7e17f78a5be758f97
Shawn Mclean <shawnrmclean@gmail.com>
2013-09-03 13:49:16 -0500

    Added core.service

---
+++ src/OnionArchitecture.Core.Service/IUserService.cs
@@ -0,0 +1,10 @@
+﻿using OnionArchitecture.Core.Domain;
+
+namespace OnionArchitecture.Core.Service
+{
+    public interface IUserService
+    {
+        void CreateUser(User user);
+        User GetUser(int userId);
+    }
+}
