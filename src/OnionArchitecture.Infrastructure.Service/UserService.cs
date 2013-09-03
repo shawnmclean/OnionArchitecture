126d617dcb8b8773e0610a6ffe5a73ee39bad313
Shawn Mclean <shawnrmclean@gmail.com>
2013-09-03 11:45:52 -0500

    More architectural work and adding user service

---
+++ src/OnionArchitecture.Infrastructure.Service/UserService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OnionArchitecture.Common;
+using OnionArchitecture.Core.Repository;
+using OnionArchitecture.Service;
+
+namespace OnionArchitecture.Infrastructure.Service
+{
+    public class UserService : IUserService
+    {
+        private readonly IUnitOfWork unitOfWork;
+        private readonly IGenericRepository<User> userRepository;
+
+        public UserService(IUnitOfWork unitOfWork, IGenericRepository<User> userRepository)
+        {
+            if (unitOfWork == null) throw new ArgumentNullException("unitOfWork");
+            if (userRepository == null) throw new ArgumentNullException("userRepository");
+            this.unitOfWork = unitOfWork;
+            this.userRepository = userRepository;
+        }
+
+        public void CreateUser(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public User GetUser(int userId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ test/OnionArchitecture.Tests/Core/Service/UserServiceTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace OnionArchitecture.Tests.Core.Service
+{
+    [TestClass]
+    public class UserServiceTests
+    {
+        [TestMethod]
+        public void CreateUser_Sets_CreatedDate()
+        {
+        }
+    }
+}
