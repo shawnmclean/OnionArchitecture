af14d231a124974f875faf52e31b4f44678110e9
Shawn Mclean <shawnrmclean@gmail.com>
2013-09-24 13:30:27 -0500

    Added ef db context

---
+++ src/OnionArchitecture.Infrastructure.Repository/EntityFramework/EntityFrameworkUnitOfWork.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OnionArchitecture.Core.Repository;
+
+namespace OnionArchitecture.Infrastructure.Repository.EntityFramework
+{
+    public class EntityFrameworkUnitOfWork : IUnitOfWork
+    {
+        private readonly DbContext context;
+        public void Dispose()
+        {
+            context.Dispose();
+        }
+
+        public int SaveChanges()
+        {
+            return context.SaveChanges();
+        }
+    }
+}
